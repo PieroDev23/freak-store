330218c9888614357bd19a59bbf3d7b11be014c1
Carlos Chavez <carloschave_2003_07@hotmail.com>
2024-10-04 02:12:12 -0500

    wip

---
+++ ViewModels/LoginVM.cs
@@ -0,0 +1,8 @@
+namespace freak_store.ViewModels
+{
+    public class LoginVM
+    {
+        public required string Username { get; set; }
+        public required string Password { get; set; }
+    }
+}
