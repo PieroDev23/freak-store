bf1dc79f55a818a6668fbafd19b0143ee941e66c
PieroDev23 <pierodavilaaguirre22@gmail.com>
2024-11-02 01:56:48 -0500

    feat: fix api

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using freak_store.ViewModels;
 using freak_store.Data;
 using Microsoft.EntityFrameworkCore;
 using System.Text.Json;
+using freak_store.Services;
 
 namespace freak_store.Controllers;
 
@@ -11,25 +12,26 @@ public class HomeController : Controller
 {
     private readonly ApplicationDbContext _context;
 
-    public HomeController(ApplicationDbContext context)
+
+    public HomeController(
+        ApplicationDbContext context
+       )
     {
         _context = context;
     }
 
-    public async Task<IActionResult> Index()
+    public IActionResult Index()
     {
         var products = _context.DataProducts
                                .Include(p => p.Inventory)
                                .Include(p => p.Category)
                                .ToList();
 
-        var featuredProducts = await GetFeaturedProducts();
 
         // Crear el modelo de vista y pasar ambos conjuntos de datos
         var viewModel = new ProductsViewModel
         {
             Products = products,
-            FeaturedProducts = featuredProducts
         };
 
         return View(viewModel);
@@ -39,14 +41,6 @@ public class HomeController : Controller
     public IActionResult Privacy()
     {
         return View();
-    }
-
-    public async Task<List<Dictionary<string, object>>> GetFeaturedProducts()
-    {
-        var client = new HttpClient();
-        var response = await client.GetStreamAsync("https://www.cheapshark.com/api/1.0/deals?storeID=1&upperPrice=5");
-        var featuredProducts = await JsonSerializer.DeserializeAsync<List<Dictionary<string, object>>>(response);
-        return featuredProducts!;
     }
 
 
--- ViewModels/HomeViewModel.cs
+++ ViewModels/HomeViewModel.cs
@@ -3,8 +3,7 @@ using freak_store.Models;
 namespace freak_store.ViewModels;
 
 public class ProductsViewModel
-{ 
-  public required List<Product> Products { get; set;}
+{
+  public required List<Product> Products { get; set; }
 
-  public required List<Dictionary<string, object>> FeaturedProducts { get; set; }
 }
