86f56f12821078bb0e1c8d94d1e9f057c7b55b6b
Carlos Chavez <carloschave_2003_07@hotmail.com>
2024-11-22 01:00:51 -0500

    agregrando IA

--- Controllers/ContactController.cs
+++ Controllers/ContactController.cs
@@ -1,6 +1,7 @@
 using freak_store.Data;
 using freak_store.Models;
 using Microsoft.AspNetCore.Mvc;
+using MLModeloContact;
 using System;
 
 namespace freak_store.Controllers
@@ -20,24 +21,48 @@ namespace freak_store.Controllers
             return View();
         }
 
-        // Acción para procesar el formulario de contacto
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public IActionResult SubmitContact(Contact contact)
+        public IActionResult SubmitContact(Contact model)
         {
-            if (ModelState.IsValid)
+            if (!ModelState.IsValid)
             {
-                // Convertir CreatedAt al formato UTC antes de guardar en la base de datos
-                contact.CreatedAt = contact.CreatedAt.ToUniversalTime();
-
-                _context.Contacts.Add(contact);
-                _context.SaveChanges();
-
-                TempData["SuccessMessage"] = "Tu mensaje ha sido enviado exitosamente.";
+                TempData["ErrorMessage"] = "Hubo errores al enviar el formulario";
                 return RedirectToAction("Index");
             }
 
-            return View("Index", contact);
+            if (!model.Message.EndsWith("."))
+            {
+                model.Message += ".";
+            }
+
+            // Crear la entrada de predicción basada en el mensaje
+            var sampleData = new MLModelContact.ModelInput()
+            {
+                Comentario = model.Message
+            };
+
+            // Realizar la predicción
+            var output = MLModelContact.Predict(sampleData);
+            var sortedScoresWithLabel = MLModelContact.PredictAllLabels(sampleData);
+
+            // Asignación de resultado al Subject basado en el score y etiquetas    
+            var scoreKeyFirst = sortedScoresWithLabel.First().Key;
+            var scoreValueFirst = sortedScoresWithLabel.First().Value;
+
+            model.Subject = scoreKeyFirst == "1" && scoreValueFirst > 0.5 ? "Positivo" : "Negativo";
+
+            Console.WriteLine($"{scoreKeyFirst,-40}{scoreValueFirst,-20}");
+            model.CreatedAt = model.CreatedAt.ToUniversalTime();
+            // Guardar el modelo en la base de datos
+            _context.Add(model);
+            _context.SaveChanges();
+
+            // Mensaje de éxito para la vista
+            TempData["SuccessMessage"] = "¡Gracias por enviarnos un mensaje, nosotros nos pondremos en contacto contigo!";
+
+            // Limpiar el modelo del formulario
+            return RedirectToAction("Index");
         }
     }
 }
